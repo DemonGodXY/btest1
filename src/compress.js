b2aa8cb7cab8f958c3dbd0d08bdb1490379a844c
DemonGodXY <gfxks457@proton.me>
2024-06-08 16:39:20 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -1,27 +1,31 @@
-const sharp = require('sharp')
-const redirect = require('./redirect')
+const sharp = require('sharp');
+const redirect = require('./redirect');
 
 function compress(req, res, input) {
-  const format = req.params.webp ? 'webp' : 'jpeg'
+  const format = req.params.webp ? 'webp' : 'jpeg';
+  let compressionQuality = req.params.quality * 0.1;
+       
+  req.params.quality = Math.ceil(compressionQuality);
 
-  sharp(input)
+
+     sharp(input)
     .grayscale(req.params.grayscale)
     .toFormat(format, {
       quality: req.params.quality,
-      progressive: true,
-      optimizeScans: true
+      effort: 0
+      
     })
     .toBuffer((err, output, info) => {
-      if (err || !info || res.headersSent) return redirect(req, res)
+      if (err || !info || res.headersSent) {
+        return redirect(req, res);
+      }
 
-      res.setHeader('content-type', `image/${format}`)
-      res.setHeader('content-length', info.size)
-      res.setHeader('x-original-size', req.params.originSize)
-      res.setHeader('x-bytes-saved', req.params.originSize - info.size)
-      res.status(200)
-      res.write(output)
-      res.end()
-    })
+      res.setHeader('content-type', `image/${format}`);
+      res.setHeader('content-length', info.size);
+      res.setHeader('x-original-size', req.params.originSize);
+      res.setHeader('x-bytes-saved', req.params.originSize - info.size);
+      res.status(200).send(output);
+    });
 }
 
-module.exports = compress
+module.exports = compress;
